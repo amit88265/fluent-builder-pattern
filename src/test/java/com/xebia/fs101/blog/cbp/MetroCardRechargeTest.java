9c08941202c6dcbc165fd63eab30d9c5fedc6ec7
amit88265 <amitkumar88265@gmail.com>
2019-10-30 16:38:35 +0530

    initial commit

--- src/test/java/com/xebia/fs101/blog/cbp/MetroCardRechargeTest.java
+++ src/test/java/com/xebia/fs101/blog/cbp/MetroCardRechargeTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 public class MetroCardRechargeTest {
 
     @Test
-    public void should_create_a_recharge_object(){
+    public void should_create_a_recharge_object() {
         // no restriction can make mistake like below
-        MetroCardRecharge recharge=new MetroCardRecharge.Builder()
+        MetroCardRecharge recharge = new MetroCardRecharge.Builder()
                 .setAmount(556)
-                .setPromoCode( "MONTHLY100" )
+                .setPromoCode("MONTHLY100")
                 .build();
-        Assertions.assertThat( recharge ).isNotNull();
+        Assertions.assertThat(recharge).isNotNull();
     }
 }
